a2b5a5c05a7569e8e39dda0613dd74483ed1930f
vidomarkas <vidomarkas@gmail.com>
2020-06-17 04:35:46 +0100

    style header

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { GiSteeringWheel } from "react-icons/gi";
 import headerImg from "../../assets/img/header-img.jpg";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-text">
-        <h1 className="header-heading">
+      <div className="header__text">
+        <ul className="header__tags">
+          <li className="header__tag">Featured</li>
+          <li className="header__tag">Best priced</li>
+          <li className="header__tag">Most popular</li>
+        </ul>
+        <h1 className="header__heading">
           The smarter way to buy a new or used car{" "}
         </h1>
-        <p className="header-subtext">
+        <p className="header__subheading">
           Great offers from all around the United Kingdom
         </p>
       </div>
+      <div
+        className="header__image"
+        style={{ backgroundImage: `url(${headerImg})` }}
+      ></div>
     </div>
   );
 };
