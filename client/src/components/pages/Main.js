29a3ca2a0baf4ed224dd7f65dc9fe9da8f288f84
vidomarkas <vidomarkas@gmail.com>
2020-06-17 22:21:07 +0100

    style search form

--- client/src/components/pages/Main.js
+++ client/src/components/pages/Main.js
@@ -1,13 +1,13 @@
 import React from "react";
 import FoundAds from "../ads/FoundAds";
-import AdSearch from "../ads/AdSearch";
+
 import Header from "../layout/Header";
 
 const Main = () => {
   return (
     <>
       <Header />
-      <AdSearch />
+
       <FoundAds />
     </>
   );
