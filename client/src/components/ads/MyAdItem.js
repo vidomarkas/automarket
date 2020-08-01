7811c0c75d031a7c8f02948d74999a4448bd93f9
vidomarkas <vidomarkas@gmail.com>
2020-08-01 10:47:55 +0100

    responsive search results

--- client/src/components/ads/MyAdItem.js
+++ client/src/components/ads/MyAdItem.js
@@ -30,37 +30,39 @@ const MyAdItem = ({ myAd }) => {
 
   return (
     <div className="my-car">
-      <div
-        className="my-car__image"
-        style={{
-          backgroundImage: imageURL
-            ? `url(${imageURL})`
-            : `url(${placeholderCar})`,
-        }}
-      ></div>
+      <div className="my-car__container">
+        <div
+          className="my-car__image"
+          style={{
+            backgroundImage: imageURL
+              ? `url(${imageURL})`
+              : `url(${placeholderCar})`,
+          }}
+        ></div>
 
-      <div className="my-car__main">
-        <div>
-          {make} {model}
+        <div className="my-car__main">
+          <div>
+            {make} {model}
+          </div>
+          <span> {dateManufactured}</span>
+          <span className="my-car__main__featured">
+            {" "}
+            {featured ? "featured" : null}
+          </span>
         </div>
-        <span> {dateManufactured}</span>
-        <span className="my-car__main__featured">
-          {" "}
-          {featured ? "featured" : null}
-        </span>
-      </div>
-      <div className="my-car__date-added">
-        Added on: {displayDate()}{" "}
-        <div className="my-car__status">
-          Status:{" "}
-          {sold ? (
-            <span className="my-car__sold">sold</span>
-          ) : (
-            <span className="my-car__for-sale">for sale</span>
-          )}
+        <div className="my-car__date-added">
+          Added on: {displayDate()}{" "}
+          <div className="my-car__status">
+            Status:{" "}
+            {sold ? (
+              <span className="my-car__sold">sold</span>
+            ) : (
+              <span className="my-car__for-sale">for sale</span>
+            )}
+          </div>
         </div>
+        <div className="my-car__price">{displayPrice(price)}</div>
       </div>
-      <div className="my-car__price">{displayPrice(price)}</div>
       <div className="my-car__controls">
         <Link className="btn btn-secondary" to={`/cars/${_id}`}>
           Visit ad
